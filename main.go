cd115730a9750471ffc471d9e2e2c31b85aea401
TABernikov <tbernikov@gmail.com>
2024-02-01 08:55:39 +0300

    new file:   DBFiles/datafrom01022024.sql 	new file:   main.exe 	modified:   main.go

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+const (
+	mainIp       = "192.168.0.139:2020"
+	testIp       = "127.0.0.1:8080"
+	mainPostgres = "tbase"
+	testPostgres = "tbasetestingdata"
+)
+
 func main() {
 
 	log.Println("hello")
@@ -16,7 +23,7 @@ func main() {
 	ctx := context.Background()
 	var jwtKey = []byte("Kr7yRk7Akv3LaN2G11-Adrrr9on5iyG1djj1K4ola5ider-hfdhswkjuIOUGFedyfhKiFGIoy")
 
-	a, err := Application.NewApp(ctx, jwtKey, "postgres", "Superkim61", "localhost:5432", "tbase", "127.0.0.1:8080")
+	a, err := Application.NewApp(ctx, jwtKey, "postgres", "Superkim61", "localhost:5432", mainPostgres, mainIp)
 	if err != nil {
 		panic(err)
 	}
